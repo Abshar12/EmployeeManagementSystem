5d2adfe3d1b9d7bb5b003ef2f16cde1c18019056
rajapc <abshar.abshar3726@gmail.com>
2022-08-23 10:58:19 +0530

    initial commit

---
+++ employee/models.py
@@ -0,0 +1,125 @@
+from django.contrib.auth.models import AbstractBaseUser ,BaseUserManager , UserManager
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+
+
+
+
+# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+class State(models.Model):
+    country = models.ForeignKey(Country,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class City(models.Model):
+    state = models.ForeignKey(State,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Gender(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class Employee(models.Model):
+    first_name = models.CharField (max_length=50)
+    last_name = models.CharField (max_length=50)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE , null=True)
+    email = models.CharField (max_length=50)
+    password = models.CharField (max_length=800)
+    address = models.CharField (max_length=50)
+    country = models.ForeignKey(Country ,on_delete=models.SET_NULL , null=True)
+    state = models.ForeignKey(State ,on_delete=models.SET_NULL , null=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL , null=True)
+    pincode = models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+    is_deleted = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.first_name 
+    
+    
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+         
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email,date_of_birth,password=None,):
+       
+        user = self.create_user(email,password=password,date_of_birth=date_of_birth,)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class Admin(AbstractBaseUser,PermissionsMixin):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField( max_length=50)
+    email = models.EmailField(verbose_name='email address',max_length=255,unique=True,)
+    date_of_birth = models.DateField()
+    created_at=models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at=models.DateTimeField(auto_now=True,blank=True,null=True)
+    # role = models.ForeignKey(Role, on_delete=models.CASCADE,default=False)
+    is_deleted =  models.BooleanField(default=0)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+       return True
+
+    @property
+    def is_staff(self):
+       return self.is_admin
+
+
+
+
+
+
+
+
+
+    
+    
---
+++ employee/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import *
+from django.contrib.auth import logout , authenticate , login 
+from django.shortcuts import render , redirect
+from cryptography.fernet import Fernet
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+
+def index(request):
+    return render(request , 'index.html')
+
+
+def admin_login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = authenticate(request,email=email,password=password)
+        print ( user is not None)
+        if user is not None:
+            login(request,user)
+            return render(request,'welcome.html')
+
+        
+    else:
+        return render(request,'admin_login.html')
+    
+    return render(request,'admin_login.html')
+
